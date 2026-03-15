c1ac22b6f1944f93e2fb1112b59de54aea942ae1
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-03-15 17:35:28 -0300

    feat: add Quick Outline asset and item pickup feedback
    Integrated Quick Outline for item highlighting and visual feedback. Items now display an outline when interactable, which is disabled upon pickup.
    
    Added item pickup sound effect and assigned it in PlayerInteraction. Updated Items.asmdef to reference QuickOutline, removed unused ItemSO.cs, and improved Gym.unity UI hierarchy and naming.

--- Assets/Features/Items/Scripts/Item.cs
+++ Assets/Features/Items/Scripts/Item.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
+using ThirdParty.QuickOutline;
 
 namespace FifthSemester.Items {
-    public class Item : MonoBehaviour, IInteractable
-    {
+    public class Item : MonoBehaviour, IInteractable {
         public bool IsInteractable => true;
+        [SerializeField] private Outline _outline;
 
-        public void Interact()
-        {
-            Debug.Log("Pode Interagir com o Item!");
-              Destroy(gameObject);
+        private void Awake() {
+            _outline = GetComponent<Outline>();
+            _outline.enabled = true;
         }
 
-        public void StopInteract()
-        {
+        public void Interact() {
+            Debug.Log("Pode Interagir com o Item!");
+            _outline.enabled = false;
+            Destroy(gameObject);
+        }
+
+        public void StopInteract() {
             Debug.Log("Não pode interagir com o Item!");
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             var itemName = gameObject.name ?? "Unnamed Item";
             return $"{itemName}";
         }

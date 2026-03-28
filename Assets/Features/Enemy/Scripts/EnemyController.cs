feb86cdbf83b0446b6d297e02bbd1f2d953b4249
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-03-28 12:56:31 -0300

    feat(enemy): add enemy AI with chase range and NavMesh updates
    - Added stopping distance and detection range to EnemyController.
    
    Enemy now only chases player within range and stops otherwise.
    
    - Added Gizmos for range visualization.
    
    - Updated NavMeshScene: added NavMeshSurface to Plane, set Enemy NavMeshAgent stopping distance to 1, disabled auto-braking, added Rigidbody, and serialized EnemyController parameters.

--- Assets/Features/Enemy/Scripts/EnemyController.cs
+++ Assets/Features/Enemy/Scripts/EnemyController.cs
@@ -8,10 +8,14 @@ namespace FifthSemester.Enemy {
         private Transform _playerTransform;
 
         [SerializeField, Range(0f, 10f)] private float _speed = 1.5f;
+        [SerializeField, Range(0f, 10f)] private float _stoppingDistance = 1f;
+        [SerializeField, Range(0f, 25f)] private float _range = 5f;
+
 
         private void Awake() {
             _navMeshAgent = GetComponent<NavMeshAgent>();
             _navMeshAgent.speed = _speed;
+            _navMeshAgent.stoppingDistance = _stoppingDistance;
         }
 
         private void Start() {
@@ -19,13 +23,42 @@ namespace FifthSemester.Enemy {
         }
 
         private void Update() {
-            SetAgentDestination();
+            if (IsPlayerInRange()) {
+                SetAgentDestination();
+            }
+            else {
+                _navMeshAgent.ResetPath();
+            }
         }
 
         private void SetAgentDestination() {
             if (_playerTransform != null) {
                 _navMeshAgent.SetDestination(_playerTransform.position);
             }
+        }
+
+        private bool IsPlayerInRange() {
+            if (_playerTransform == null) return false;
+
+            float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
+            return distanceToPlayer <= _range;
+        }
+
+        private void OnDrawGizmos() {
+            if (_playerTransform == null) return;
+
+            GizmosPlayerDistance();
+            GizmosEnemyRange();
+        }
+        private void GizmosPlayerDistance() {
+            if (!IsPlayerInRange()) return;
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, _playerTransform.position);
+        }
+        private void GizmosEnemyRange() {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _range);
         }
     }
 }

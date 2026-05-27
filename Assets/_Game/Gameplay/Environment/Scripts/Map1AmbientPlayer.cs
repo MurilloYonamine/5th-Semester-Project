27d5e39ad5c3fb87705d6bf74ed8afbed8271911
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-05-27 19:32:45 -0300

    feat: add ambient audio player and associated audio files for Map 1

---
+++ Assets/_Game/Gameplay/Environment/Scripts/Map1AmbientPlayer.cs
@@ -0,0 +1,39 @@
+using FifthSemester.Core.Services;
+using UnityEngine;
+
+namespace FifthSemester.Gameplay.Environment {
+    public class Map1AmbientPlayer : MonoBehaviour {
+        [Header("Ambient Settings")]
+        [Tooltip("Ambient audio clip for Map 1.")]
+        [SerializeField] private AudioClip _ambientClip;
+
+        [Tooltip("Playback volume (0-1).")]
+        [Range(0f, 1f)]
+        [SerializeField] private float _volume = 1f;
+
+        [SerializeField] private bool _loop = true;
+        [SerializeField] private bool _playOnStart = true;
+
+        private IAudioService _audioService;
+
+        private void Awake() {
+            _audioService = ServiceLocator.Get<IAudioService>();
+        }
+
+        private void Start() {
+            if (_playOnStart) Play();
+        }
+
+        public void Play() {
+            if (_ambientClip == null) return;
+
+            _audioService.PlayAmbience(_ambientClip, startingVolume: 1f, loop: true);
+        }
+
+        public void OnDestroy() {
+            if (_audioService != null) {
+                _audioService.StopAmbience(_ambientClip);
+            }
+        }
+    }
+}
